a6dd7659ffb274c557c6342933aa6bbc111e4a93
Kigazine <lien.jessie@gmail.com>
2026-05-20 21:18:14 +0800

    Move Firebase config to root and set both admin emails

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// 1) Replace this with your Firebase Web App config.
+// 2) Keep this file in the same folder as the HTML pages.
+export const firebaseConfig = {
+  apiKey: "PASTE_YOUR_API_KEY",
+  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
+  projectId: "PASTE_YOUR_PROJECT_ID",
+  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "PASTE_YOUR_SENDER_ID",
+  appId: "PASTE_YOUR_APP_ID"
+};
+
+// Club admins allowed into admin.html.
+export const ADMIN_EMAILS = [
+  "lev-losif01px2035@saschina.org",
+  "ethan02px2035@saschina.org"
+];
